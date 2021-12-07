ee6bb1309b8ce984220b63a2e38addae4ca91a70
Ede Meijer <ede@elastict.nl>
2021-12-07 08:54:11 +0100

    Day 7

---
+++ Aoc2021/Day7.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+using Aoc2021.Lib;
+
+namespace Aoc2021
+{
+    public class Day7
+    {
+        public static long Part1() => Solve(d => d);
+
+        public static long Part2() => Solve(d => d * (d + 1) / 2);
+
+        private static long Solve(Func<long, long> calcFuel)
+        {
+            var crabs = Input.Text(7).Trim().Split(',').Select(int.Parse).ToArray();
+            return Enumerable.Range(crabs.Min(), crabs.Max() - crabs.Min() + 1)
+                .Min(pos => crabs.Select(c => calcFuel(Math.Abs(pos - c))).Sum());
+        }
+    }
+}
