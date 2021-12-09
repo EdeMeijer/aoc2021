7ba997832bdf26d1477b159a10ba600898e8dd13
Ede Meijer <ede@elastict.nl>
2021-12-09 09:55:31 +0100

    Add some timing to output

--- Aoc2021/Program.cs
+++ Aoc2021/Program.cs
@@ -1,5 +1,13 @@
 ﻿using System;
+using System.Diagnostics;
 using Aoc2021;
 
-Console.WriteLine(Day9.Part1());
-Console.WriteLine(Day9.Part2());
+Console.WriteLine("======== PART 1 ========");
+var sw = Stopwatch.StartNew();
+Console.WriteLine($"Answer = {Day9.Part1()}");
+Console.WriteLine($"Runtime = {sw.ElapsedMilliseconds}ms");
+
+Console.WriteLine("======== PART 2 ========");
+sw.Restart();
+Console.WriteLine($"Answer = {Day9.Part2()}");
+Console.WriteLine($"Runtime = {sw.ElapsedMilliseconds}ms");
